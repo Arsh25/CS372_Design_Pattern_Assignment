97a10983aa32b34e6c65474b21bb23fe22837977
Arsh Chauhan <achauhan@alaska.edu>
2016-04-27 21:55:46 -0800

    Triedn to make a server, crashing

--- serverMain.cpp
+++ serverMain.cpp
@@ -1,6 +1,29 @@
 #include "server.hpp"
 
+#include <iostream>
+using std::cout;
+using std::endl;
+
 int main()
 {
-	return 0; //Dummy return 
+	server mainServer;
+
+	mainServer.create();
+
+	std::vector<string> clients = mainServer.getClients();
+	//std::vector<string> messages = mainServer.getMessages();
+
+	while (true)
+	{
+		mainServer.addClient();
+
+		for (int i=0; i<clients.size(); i++)
+		{
+			cout << "client: "<<i<<endl;
+
+		}
+
+	}
+
+	return 0; 
 }
