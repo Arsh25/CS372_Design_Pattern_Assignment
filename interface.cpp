36bba1b4ea60f7f4469579d5223547f96369d402
Arsh Chauhan <achauhan@alaska.edu>
2016-05-02 12:59:35 -0800

    Removed files for initial idea of a web proxy

--- interface.cpp
+++
@@ -1,8 +0,0 @@
-#include "interface.hpp"
-
-interface::interface( SOCKET listenPort)
-{
-	listenPort_ = listenPort;
-	string host = "0.0.0.0" + std::to_string(listenPort_);
-	server_ = msl::socket (host);
-}
