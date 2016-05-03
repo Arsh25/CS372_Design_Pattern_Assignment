a54dec3652b4783d983fbef91b6194ff20411ae2
Arsh Chauhan <achauhan@alaska.edu>
2016-05-02 17:29:39 -0800

    Added commenst to clarify why standard libraries are included in base class

--- image.hpp
+++ image.hpp
@@ -4,7 +4,7 @@
 	05/02/2016
 
 	CS372 Assignment 4
-	Interface for proxy pattern design pattern.
+	Interface for image viewer.
 
 	No associated source file.
 */
@@ -12,6 +12,10 @@
 #ifndef IMAGE_HPP
 #define IMAGE_HPP
 
+/*
+	Added theses here to prevent having
+	to add them to both serived class headers
+*/
 #include <string>
 using std::string;
 #include <iostream>
