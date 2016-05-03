baa3296008d08e3e81d194bdbbca3e3905ec4004
Arsh Chauhan <achauhan@alaska.edu>
2016-05-02 17:16:56 -0800

    Added pre and post conditions

--- realImage.cpp
+++ realImage.cpp
@@ -11,17 +11,34 @@
 Realimage::Realimage ()
 {}
 
+/*
+	2 parameter constructor
+	Pre: None
+	Post: Creates new Realimage with filename fileName
+*/
 Realimage::Realimage (string fileName): fileName_(fileName)
 {
 	cout << "Real Image created " << endl;
 	loadImage();
 } 
 
+/*
+	display
+	Pre: None
+	Post: Prints to stdout 
+		Theoretically: Displays an image 
+*/
 void Realimage::display()
 {
 	cout << "Displaying Image" << fileName_ << endl;
 }
 
+/*
+	loadImage
+	Pre: None
+	Post: Prints to stdout 
+		Theoretically: Loads an image from disk 
+*/
 void Realimage::loadImage()
 {
 	cout << "Loaded Image " << fileName_ << " from disk" <<endl;
