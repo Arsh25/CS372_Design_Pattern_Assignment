9b3f6ebd444380cae35b7765ffbfeb6763ab420b
Arsh Chauhan <achauhan@alaska.edu>
2016-05-02 17:21:36 -0800

    Added pre and post conditions

--- proxyImage.cpp
+++ proxyImage.cpp
@@ -8,11 +8,23 @@
 
 #include "proxyImage.hpp"
 
+/*
+	2 parameter constructor
+	Pre: None
+	Post: Creates new Proxyimage with filename fileName
+*/
 Proxyimage::Proxyimage(string fileName): fileName_(fileName)
 {
 	cout << "Proxy image created for file " << fileName << endl;
 }
 
+/*
+	display
+	Pre: None
+	Post: Creates a bew RealImage with filename 
+	         fileName_ and sets realImage_ to that object
+		  Calls RealImage's display function with realImage_
+*/
 void Proxyimage::display()
 {
 	realImage_ = Realimage(fileName_);
