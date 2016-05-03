e1bdcf39a5b8e5f792d0829c0439bc2f662af92a
Arsh Chauhan <achauhan@alaska.edu>
2016-05-02 17:29:50 -0800

    Added comments

--- main.cpp
+++ main.cpp
@@ -1,21 +1,37 @@
+/*
+	main.cpp
+	Arsh Chauhan
+	05/02/2016
+
+	Implements theretical image viewer designed
+	using the proxy design pattern
+*/
 #include "realImage.hpp"
 #include "proxyImage.hpp"
 
 int main()
 {
+	/*
+		Create and display a Realimage. Used to 
+		demonstarte how Realimage is different from
+		Proxyimage
+	*/
 	cout <<"CREATE AND DISPLAY REAL IMAGE " << endl;
-	Realimage test01 ("test01.jpg");
+	Realimage test01 ("test01.jpg"); // Image loaded when created
 	test01.display();
 
-	cout <<endl;
+	cout <<endl; // Make output clearer 
+
 	cout <<"CREATING PROXY IMAGES" << endl;
+	// Image not loaded when created
 	Proxyimage proxyTest01 ("proxyTest01.jpg");
 	Proxyimage proxyTest02 ("proxyTest02.jpg");
 	Proxyimage proxyTest03 ("proxyTest03.jpg");
 
-	cout <<endl;
+	cout <<endl; // Make output clearer
 
 	cout << "DISPLAYING PROXY IMAGES" <<endl;
+	//Image loaded when display is called
 	proxyTest01.display();
 	proxyTest03.display();
 	proxyTest02.display();
